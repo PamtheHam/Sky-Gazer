bdd923e1326cff520132d1a58a40d4b4eb1fe20d
Lauren Goss <laurenlgoss98@gmail.com>
2021-07-17 13:44:08 -0400

    fetch weather data from lat/lon

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,9 @@
+const currentDate = moment();
+
+function init() {
+    fetchLatLon("Raleigh");
+}
+
 // Fetch all norad-IDs
     // Render to page
 
@@ -6,13 +12,34 @@
 
 // Get user input: city, norad-ID
     // Convert user input (city) to lat/lon
+function fetchLatLon(cityInput) {
+    fetch("https://api.openweathermap.org/geo/1.0/direct?q=" + cityInput + "&limit=1&appid=c8aa884e6f28d929f55e9ba1856815bd")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            var lat = data[0].lat;
+            var lon = data[0].lon;
+
+            fetchWeather(lat, lon);
+        })
+}
 
     // Fetch satellite data using lat/lon, norad-ID
         // Render next X satellites to page as buttons
 
         // Add event listeners to each satellite button
             // Fetch weather data using lat/lon and date
-                // Get weather icon
+function fetchWeather(lat, lon) {
+    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly,alerts&units=imperial&appid=c8aa884e6f28d929f55e9ba1856815bd")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            // Get weather icon
+            console.log(data);
+        })
+}
 
                 // Render weather icon
 
@@ -22,3 +49,5 @@
 
         // If not favorable,
             // Render "not visible"
+
+init();
